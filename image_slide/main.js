4549facbcc87c7fcbb2afe63f9ea488981c16ee7
PinkPaca <fmadkdl113@gmail.com>
2023-08-07 11:26:06 +0100

    image slide

---
+++ image_slide/main.js
@@ -0,0 +1,25 @@
+
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+prevBtn.addEventListener("click", ()=>{
+  const currentImg = document.querySelector(".active");
+  var imgId = currentImg.id;
+
+  var prevImgId = imgId - 1 > 0 ?imgId - 1: 4;
+
+  currentImg.classList.remove("active");
+  const prevImg = document.getElementById(prevImgId);
+  prevImg.classList.add("active");
+});
+
+nextBtn.addEventListener("click", ()=>{
+  const currentImg = document.querySelector(".active");
+  var imgId = parseInt(currentImg.id)
+
+  const nextImgId = imgId + 1 < 5 ? imgId + 1: 1;
+
+  currentImg.classList.remove("active");
+  const nextImg = document.getElementById(nextImgId);
+  nextImg.classList.add("active");
+});
